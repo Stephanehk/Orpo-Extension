17d50755f7f97b8b68e41d224ea8ecc38473ca3c
Stephane Hatgis-Kessell <stephhk@next5.stanford.edu>
2025-05-01 09:38:19 -0700

    reg. update

--- slurm_scripts/train_no_reg.sh
+++ slurm_scripts/train_no_reg.sh
@@ -3,4 +3,4 @@ cd /next/u/stephhk/orpo/
 conda activate orpo
 
 
-python -m occupancy_measures.experiments.orpo_experiments with env_to_run=pandemic reward_fun=proxy exp_algo=ORPO om_divergence_coeffs=[0.0] seed=0 num_rollout_workers=2 num_gpus=1
+python -m occupancy_measures.experiments.orpo_experiments with env_to_run=pandemic checkpoint_to_load_current_policy=data/logs/pandemic/BC/true/model_128-128/seed_0/2025-04-28_09-41-36/checkpoint_000260 reward_fun=proxy exp_algo=ORPO om_divergence_coeffs=[0.0] seed=0 num_rollout_workers=2 num_gpus=1
